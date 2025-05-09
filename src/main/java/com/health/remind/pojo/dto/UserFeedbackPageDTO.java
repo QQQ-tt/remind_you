c4c776c76384e24399a6847ee376188538e60220
QQQtx <1102214883@qq.com>
2025-05-09 12:34:13 +0800

    feat:添加注解

--- src/main/java/com/health/remind/pojo/dto/UserFeedbackPageDTO.java
+++ src/main/java/com/health/remind/pojo/dto/UserFeedbackPageDTO.java
@@ -3,12 +3,14 @@ package com.health.remind.pojo.dto;
 import com.health.remind.config.PageDTO;
 import com.health.remind.entity.UserFeedback;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author qtx
  * @since 2025/5/7 16:29
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class UserFeedbackPageDTO extends PageDTO<UserFeedback> {
 
     private String title;
